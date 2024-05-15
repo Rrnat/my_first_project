b7d29f738697070422bd9504de8fbf743b24c0f5
твой нRrnat <@mamedov-rf@mail.com>
2024-05-15 06:21:29 +0500

    first commit

---
+++ config.py
@@ -0,0 +1,26 @@
+TOKEN = '6906971121:AAGOz4GmvxjtPBCt3Bifxz_r0nEaSoAdOO4'
+
+MAX_USERS = 3
+MAX_GPT_TOKENS = 120
+COUNT_LAST_MSG = 4
+
+IAM_TOKEN = "t1.9euelZqNjJrKl4mJi56cjpWUypyLiu3rnpWayJeVkZCKis_NkJbOjsuam4_l8_dtIXBN-e9iJGtL_d3z9y1QbU3572Ika0v9zef1656VmpqLmYqeiZHGmZaQkZSVzZPP7_zF656VmpqLmYqeiZHGmZaQkZSVzZPPveuelZqXjZWPypmWzJvHkZ2PzpqZkrXehpzRnJCSj4qLmtGLmdKckJKPioua0pKai56bnoue0oye.JextKLgFpU-6OY_vzul-OYLPPnQDcaF6dFPCQj4mBB8G5_MpIapHzlAjNEEfBrA2k5tGxDuJsBbg2nV_JQNrCQ"
+FOLDER_ID = 'b1gtcn1fr80ocof1599a'
+ADMIN_ID = '801460635'  # user_id автора бота чтобы получать отчеты с логами
+
+
+MAX_USER_STT_BLOCKS = 10
+MAX_USER_TTS_SYMBOLS = 5_000
+MAX_USER_GPT_TOKENS = 2_000
+MAX_TTS_SYMBOLS = 12
+
+SYSTEM_PROMPT = [{'role': 'system', 'text': 'Ты профессиональный психолог.'
+                                            'Помоги человеку разобраться в себе.'}]
+
+HOME_DIR = '/Users/Citi/Desktop/pythonYandex/BotZinka'
+LOGS = f'{HOME_DIR}/logs.txt'
+DB_FILE = f'{HOME_DIR}/messages.db'
+
+IAM_TOKEN_PATH = f'{HOME_DIR}/creds/iam_token.txt'
+FOLDER_ID_PATH = f'{HOME_DIR}/creds/folder_id.txt'
+BOT_TOKEN_PATH = f'{HOME_DIR}/creds/bot_token.txt'
